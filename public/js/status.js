81986fc0d65c7bbfa0274f8d5375b56df7539094
Teba Gomez <gomez.teba@gmail.com>
2018-11-10 17:58:35 +0000

    Data template + render added. Ajax workflow done

--- public/js/status.js
+++ public/js/status.js
@@ -1,16 +1,14 @@
 var errSlctr = document.getElementById("error-data");
 var loadingSlctr = document.getElementById("loading");
+var statusSlctr = document.getElementById("status-data");
 
 function ajaxHandler (url, cb) {
     var request = new XMLHttpRequest();
-
     request.onreadystatechange = function() {
-
         if (request.readyState === 4) {
             if(request.status === 200) {
                 var data = JSON.parse(request.responseText);
                 cb(false, data);
-                console.timeEnd("AJAX");
             } else {
                 cb(true, request.status);
             }
@@ -28,9 +26,60 @@ function hideLoading () {
     loadingSlctr.style.display = "none";
 }
 
-console.time("AJAX");
+function semanticTranslator (key) {
+    var dic = {
+        "OK": {label: "success", text: "Operational"},
+        "Warning":{label: "warning", text: "Partial outage"},
+        "Critical":{label: "danger", text: "Major outage"},
+        "Unknown":{label: "dark", text: "System Down"}
+    };
+    var match = dic[key];
+    return match ? match : "dark";
+}
+
+function templateItem (item) {
+    var statusDetails = semanticTranslator(item.state.service_state);
+    return `<li class="list-group-item d-flex justify-content-between align-items-center">
+       ${item.name} <span><span class="badge badge-light">${item.output.rta_ms}ms</span> <span class="badge badge-${statusDetails.label}">${statusDetails.text}</span></span>
+    </li>`;
+}
+
+function templateInfo () {
+    return `<div class="text-center card col-md-7 mx-auto my-2">
+      <div class="card-body">
+            <p class="card-text">We continuously monitor Open Source Weekends systems and projects.
+            If there are any performance or service interruptions, an update will be posted here.</p>
+        </div>
+    </div>`;
+}
+
+function statusItemsDetails (data) {
+    return data.map(function (item){
+        return templateItem(item);
+    }).join("");
+}
+
+function statusTemplate (data) {
+    return `
+<div class="container">
+   <div class="row">
+        ${templateInfo()}
+      <div class="list col-md-8 mx-auto my-5">
+         <ul class="list-group list-group-flash">
+            <li class="list-group-item list-group-item-dark">Monitored Systems</li>
+            ${statusItemsDetails(data)}
+         </ul>
+      </div>
+   </div>
+</div>
+`;
+}
+
+function renderTemplate (data){
+    statusSlctr.innerHTML = statusTemplate(data);
+}
 
 ajaxHandler("/api/v1/host", function (err, data) {
-    err ? errShow() : console.log("Data:", data);
+    err ? errShow() : renderTemplate(data);
     hideLoading();
 });
